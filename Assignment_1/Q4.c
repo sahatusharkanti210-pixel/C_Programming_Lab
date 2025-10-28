995ab1ac3ccf8a19114cb74145d841aa05a56a25
sahatusharkanti210-pixel <sahatusharkanti210@gmail.com>
2025-10-28 20:51:43 +0530

    Add billing calculation program for electricity usage

---
+++ Assignment_1/Q4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int units;
+    float bill;
+
+    printf("Enter units consumed: ");
+    scanf("%d", &units);
+
+    if (units <= 100) {
+        bill = units * 1.5;
+    } 
+    else if (units <= 200) {
+        bill = (100 * 1.5) + ((units - 100) * 2);
+    } 
+    else if (units <= 300) {
+        bill = (100 * 1.5) + (100 * 2) + ((units - 200) * 3);
+    } 
+    else {
+        bill = (100 * 1.5) + (100 * 2) + (100 * 3) + ((units - 300) * 5);
+    }
+
+    printf("Total Bill = ?%.2f\n", bill);
+
+    return 0;
+}
